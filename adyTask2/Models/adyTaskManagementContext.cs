2967b13828d1c1ae68c724761ef9bdb67f76fa63
Nicat Mardanov <nicaattttt@hotmail.com>
2020-02-07 11:39:28 +0400

    ....

--- adyTask2/Models/adyTaskManagementContext.cs
+++ adyTask2/Models/adyTaskManagementContext.cs
@@ -47,7 +47,10 @@ namespace adyTask2.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-UTUBGGC\\SQLEXPRESS;Database=adyTaskManagement;Trusted_Connection=True;");
+                //optionsBuilder.UseSqlServer("Server=DESKTOP-UTUBGGC\\SQLEXPRESS;Database=adyTaskManagement;Trusted_Connection=True;");
+
+                optionsBuilder.UseSqlServer(@"Data Source=192.168.5.17;Initial Catalog=adyTaskManagement;User ID=meeting;Password=12345@ady;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+
 
                 optionsBuilder.UseLazyLoadingProxies();
                 optionsBuilder.ConfigureWarnings(w => w.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.CoreEventId.LazyLoadOnDisposedContextWarning));
