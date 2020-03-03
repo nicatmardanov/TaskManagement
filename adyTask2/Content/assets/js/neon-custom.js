854a0b7ec1bfb2161a1a1ec5075503bd04d5472d
Nicat Mardanov <nicaattttt@hotmail.com>
2020-03-04 00:43:37 +0200

    ,./

--- adyTask2/Content/assets/js/neon-custom.js
+++ adyTask2/Content/assets/js/neon-custom.js
@@ -1131,46 +1131,47 @@ var public_vars = public_vars || {};
 				for (var i = 0; i < length; i++) {
 					if (tile_length > 0) {
 						$($($('.tile-stats-custom')[i]).find('span')).html(notifications[i]);
-						$($($('.tile-stats-custom')[i]).find('span')).attr('data-end', notifications[i]);
+						//$($($('.tile-stats-custom')[i]).find('span')).attr('data-end', notifications[i]);
+						$($($('.tile-stats-custom')[i]).find('span')).html(notifications[i]);
 					}
 					$($($('.sidebar_ntfcts li')[i]).find('.badge')).html(notifications[i]);
 					$($($('.top_ntfcts li')[length - i - 1]).find('.badge')).html(notifications[i]);
 
 				}
 
-				$(".tile-stats-custom").each(function (i, el) {
-					var $this = $(el),
-						$num = $this.find('.num'),
+				//$(".tile-stats-custom").each(function (i, el) {
+				//	var $this = $(el),
+				//		$num = $this.find('.num'),
 
-						start = attrDefault($num, 'start', 0),
-						end = attrDefault($num, 'end', 0),
-						prefix = attrDefault($num, 'prefix', ''),
-						postfix = attrDefault($num, 'postfix', ''),
-						duration = attrDefault($num, 'duration', 1000),
-						delay = attrDefault($num, 'delay', 1000);
+				//		start = attrDefault($num, 'start', 0),
+				//		end = attrDefault($num, 'end', 0),
+				//		prefix = attrDefault($num, 'prefix', ''),
+				//		postfix = attrDefault($num, 'postfix', ''),
+				//		duration = attrDefault($num, 'duration', 1000),
+				//		delay = attrDefault($num, 'delay', 1000);
 
-					if (start < end) {
-						if (typeof scrollMonitor == 'undefined') {
-							$num.html(prefix + end + postfix);
-						}
-						else {
-							var tile_stats = scrollMonitor.create(el);
+				//	if (start < end) {
+				//		if (typeof scrollMonitor == 'undefined') {
+				//			$num.html(prefix + end + postfix);
+				//		}
+				//		else {
+				//			var tile_stats = scrollMonitor.create(el);
 
-							tile_stats.fullyEnterViewport(function () {
+				//			tile_stats.fullyEnterViewport(function () {
 
-								var o = { curr: start };
+				//				var o = { curr: start };
 
-								TweenLite.to(o, duration / 1000, {
-									curr: end, ease: Power1.easeInOut, delay: delay / 1000, onUpdate: function () {
-										$num.html(prefix + Math.round(o.curr) + postfix);
-									}
-								});
+				//				TweenLite.to(o, duration / 1000, {
+				//					curr: end, ease: Power1.easeInOut, delay: delay / 1000, onUpdate: function () {
+				//						$num.html(prefix + Math.round(o.curr) + postfix);
+				//					}
+				//				});
 
-								tile_stats.destroy()
-							});
-						}
-					}
-				});
+				//				tile_stats.destroy()
+				//			});
+				//		}
+				//	}
+				//});
 
 
 			}
